e0d5ff95c76218976f1eaa0f7a6234e4d3a9d6aa
ZacByrne <zacbyrne@gmail.com>
2017-04-13 17:11:07 +1000

    Add files via upload

---
+++ jeans.cpp
@@ -0,0 +1,59 @@
+//#include <iostream>
+//#include <fstream>
+#include <cmath>
+//#include <cassert>
+//#include <boost/lexical_cast.hpp>
+
+// NULL is gone, use nullptr
+double* me = nullptr;
+double pluma = 1;
+//GET VALUE FOR HOLGER
+
+double galmass = 1:
+double Gconst = 1;
+
+
+
+double f(double r, double y_n)
+{
+  //Insert eqn f here
+}
+
+//stellar density
+double vsteldelplum(double r)
+{
+  //need density equation?
+}
+
+//Mass Function
+double Mfuncplum(double r)
+{
+  massr = galmass * (1 + pow(pluma/r,2));
+  return massr
+}
+
+
+double dgravpotplum(double r, )
+{
+  dphi = Gconst * Mfuncplum(r) * r * pow(pluma, -3) * (1 + pow(pluma/r,2));
+  return dphi
+}
+
+
+double integrate(function f, float start, float end, float step)
+{
+  double s = 0;
+  double h = (end - start)/steps;
+  for (int i =0; i<steps; ++i)
+    s += h*
+      }
+}
+
+
+int main()
+{
+  return 0;
+
+}
+
+
