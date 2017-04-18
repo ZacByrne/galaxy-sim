113bbff0e2ef86cee31c5142d9ea179f57925dbd
ZacByrne <zacbyrne@gmail.com>
2017-04-18 15:11:55 +1000

    Update jeans.cpp

--- jeans.cpp
+++ jeans.cpp
@@ -13,14 +13,32 @@ double galmass = 1:
 double Gconst = 1;
 
 
-
-double f(double r, double y_n)
+double beta(double v_r2, double v_theta2)
 {
-  //Insert eqn f here
+  betavalue = 1 - (v_theta2)/(v_r2);
+  return betavalue;
+}
+
+double  rotvel(double r)
+{
+  //WHAT IS THE ROT VEL?
+  return 0;
+}
+
+
+double fry(double r, double y_n)
+{
+  fvalue = dgravpotplum(r)/vsteldelplum(r) - 2*beta(y_n,rotvel(r))* y_n / r - dvelstelplum*y_n/vsteldelplum(r);
+  return fvalue
 }
 
 //stellar density
 double vsteldelplum(double r)
+{
+  //need density equation?
+}
+
+double dvstelplum(double r)
 {
   //need density equation?
 }
@@ -40,14 +58,28 @@ double dgravpotplum(double r, )
 }
 
 
-double integrate(function f, float start, float end, float step)
+double integrate(double start, double end, double step)
 {
   double s = 0;
   double h = (end - start)/steps;
   for (int i =0; i<steps; ++i)
-    s += h*
-      }
+  {
+    s += h* 
+  }
 }
+
+double midpointarray(double start, double end, double step)
+{
+  float s =0;
+  float h = (end - start)/steps;
+  float yn = s;
+  for (int i =0; i<steps; ++i)
+   {
+      yn = s;
+      rn = i*h
+      s = h*fry(rn+h/2, yn + h/2*fry(rn,yn));
+      
+   }
 
 
 int main()
