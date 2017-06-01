d1bfa5419a0c387ee6d07676ddad8b0dd5c6f1e0
ZacByrne <zacbyrne@gmail.com>
2017-06-01 17:07:37 +1000

    Create eddington.cpp
    starting file

---
+++ eddington.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+double pi = 3.1415926535;
+double Gconst = 6.674*pow(10.0,-11.0);
+
+void massr(std::vector<double>& density_array, 
+
+
+
+int main(int argc, char** argv)
+{
+  // need to calc M(<r) from \rho
+
+
+}
